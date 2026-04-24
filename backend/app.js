3ae6dbcf0d6f2f0095b6b0005fefc8f1f67bd8e7
Amna <socialcircle87@gmail.com>
2026-04-24 16:08:19 +0500

    base url change

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-production-7caf.up.railway.app",
+    // origin: "http://localhost:5173",
     credentials: true,
   })
 );
